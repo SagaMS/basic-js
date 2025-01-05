5538fc1f6b1c7eb6fbb39eb3e2739ab160f40a26
SagaMS <91miablond@gmail.com>
2025-01-05 19:00:13 +0700

    feat:solve first task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || Number(sampleActivity) === NaN || sampleActivity !== sampleActivity || sampleActivity >= MODERN_ACTIVITY || sampleActivity <=0 || isNaN(sampleActivity)){
+    return false;
+}
+
+
+return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)*HALF_LIFE_PERIOD)/Math.log(2));
 }
 
 module.exports = {
