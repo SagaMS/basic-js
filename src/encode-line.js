38169ce849b80f0915f3796db64a205e34dbd3a4
SagaMS <91miablond@gmail.com>
2025-01-07 03:32:55 +0700

    feat:solve task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
+function encodeLine(str) {
+  let result = '';
+  while(str!==''){
+     const temp = makeSub(str, str[0])
+     result +=temp[0];
+     str = temp[1]
+ 
+  }
+  function makeSub(str, letter){
+     const firstLetter = str[0];
+     if(firstLetter !== letter){
+         return [letter, str]
+     }else{
+     let count = 0      
+     for (let index = 0; index < str.length; index++) {
+         if(firstLetter === str[index]){
+             count ++;  
+         }else{
+              return [`${count}${firstLetter}`, str.slice(index)]
+             // return `${count}${firstLetter}`
+         }
+          
+      }
+      return [`${count}${firstLetter}`, '']
+     }
+     }
+ 
+     let eins = "1";
+     let regex = new RegExp(eins, 'g');
+     return result.replace(regex, '');
+     
+ }
 module.exports = {
   encodeLine
 };
